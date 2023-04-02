c50c599b5c2bef31b564f89d72ca9380697d0955
juncos22 <juncosnicolas36@gmail.com>
2023-04-02 16:56:18 -0300

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -3,15 +3,7 @@ const nextConfig = {
   reactStrictMode: true,
   images: {
     domains: ['mexx-img-2019.s3.amazonaws.com']
-  },
-  async rewrites() {
-    return [
-      {
-        source: '/api/:path*',
-        destination: 'https://techshop-nine.vercel.app/:path*',
-      },
-    ]
-  },
+  }
 }
 
 module.exports = nextConfig
