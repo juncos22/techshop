7a89957b39adcaa08a7d6a8aa23f63fc03bb503a
juncos22 <juncosnicolas36@gmail.com>
2023-04-02 15:17:41 -0300

    more fixing

--- src/pages/api/products/[[...params]].ts
+++ src/pages/api/products/[[...params]].ts
@@ -12,7 +12,8 @@ export default async function handler(req: NextApiRequest, res: NextApiResponse<
         let options = {
             include: {
                 category: true
-            }
+            },
+            where: {}
         }
         if (categoryName) {
             filterOptions.push({
